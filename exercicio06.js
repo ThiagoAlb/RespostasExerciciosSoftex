7df21e829a5e27ea74c0cd8d0c24ffde426be955
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-07-24 20:11:18 +0000

    Exercício 06 - final

--- exercicio06.js
+++ exercicio06.js
@@ -1,3 +1,4 @@
+//Ativar o módulo que permite fazer perguntas pelo terminal.
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -5,30 +6,43 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
-let resp_eTerrestre;
-let resp_usaCapacete;
-let resp_quantPessoas;
-let ePesado;
-let temPedal;
 
+//Fazendo uma pergunta ao usuário
 rl.question("É terrestre? ", function(eTerrestre) {
-    resp_eTerrestre = eTerrestre;
 
-    rl.question("Usa capacete?", function(usaCapacete) {
-        resp_usaCapacete = usaCapacete;
+    rl.question("Usa capacete? ", function(usaCapacete) {
         if (usaCapacete === "Sim") {
             console.log("O transporte escolhido é uma moto.");
+            // Fechando a interface após obter a resposta
+            rl.close();
         } else {
-            rl.question("Cabe apenas uma pessoa?", function(quantPessoas) {
-                resp_quantPessoas = quantPessoas;
+            rl.question("Cabe apenas uma pessoa? ", function(quantPessoas) {
                 if(quantPessoas === "Sim") {
-                    rl.question(
-
-                    )
+                    rl.question("É pesado? ", function(ePesado) {
+                        if(ePesado === "Sim") {
+                            console.log("O transporte escolhido é um trator.");
+                            // Fechando a interface após obter a resposta
+                            rl.close();
+                        } else {
+                            rl.question("Tem pedal? ", function(temPedal) {
+                                if(temPedal === "Sim") {
+                                    console.log("O transporte escolhido é uma bicicleta.");
+                                    // Fechando a interface após obter a resposta
+                                    rl.close();
+                                } else {
+                                    console.log("Veículo não cadastrado.");
+                                    // Fechando a interface após obter a resposta
+                                    rl.close();
+                                }
+                            });
+                        }   
+                    });
                 } else {
-
+                    console.log("Seu veículo não está cadastrado!");
+                    // Fechando a interface após obter a resposta
+                    rl.close();
                 }   
-            })
+            });
         }
-    })
-})
+    });
+});
